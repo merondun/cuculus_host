e8d56c1fd477fcab4bc4e41de32711318b776dfa
merondun <heritabilities@gmail.com>
2024-10-15 01:58:17 +0200

    finalize

--- gens_associations/4.Autosomal-vs-mtDNA_CharacterShifts.R
+++ gens_associations/4.Autosomal-vs-mtDNA_CharacterShifts.R
@@ -25,6 +25,10 @@ m = read.iqtree('ml_trees/chr_MT_n89.min4.phy.varsites.phy.contree')
 m1 <- root(as.phylo(m),outgroup = '387_CP_MBW_RUS_F')
 mt_tree <- drop.tip(m1,c('387_CP_MBW_RUS_F','386_CP_MBW_RUS_M'))
 
+# Mid rooted
+m0 <- drop.tip(as.phylo(m),c('387_CP_MBW_RUS_F','386_CP_MBW_RUS_M'))
+mt_tree <- midpoint.root(m0)
+
 ### AUTOSOME tree 
 a = read.iqtree('ml_trees/autos_LD_n89.min4.phy.varsites.phy.contree')
 a1 <- root(as.phylo(a),outgroup = '387_CP_MBW_RUS_F')
@@ -129,7 +133,7 @@ for (tree in c('mt_tree','a_tree')) {
   
   # Generate base tree 
   targ_tree <- get(tree)
-  ggt <- ggtree(targ_tree, layout = "circular",branch.length='none') %<+% md 
+  ggt <- ggtree(targ_tree, layout = "rectangular",branch.length='none') %<+% md 
   
   #grab only egg
   phenos <- as.data.frame(ggt$data %>% filter(isTip == TRUE))
@@ -186,19 +190,47 @@ for (tree in c('mt_tree','a_tree')) {
     node=1:t2$Nnode+Ntip(t2),
     fitER$lik.anc)
   
+  # For stochastic mapping 
+  obj <- describe.simmap(simfull,plot=FALSE)
+  mcmc_nodes <- as.data.frame(cbind(node=rownames(obj$ace),obj$ace)); rownames(mcmc_nodes) <- NULL
+  mcmc_nodes <- mcmc_nodes %>% mutate(across(starts_with('E'), as.numeric))
+  nodes_plot <- mcmc_nodes %>% filter(node %in% nodes$node)
+  rownames(nodes_plot) <- nodes_plot$node
+  nodes_plot$node <- as.integer(nodes_plot$node)
+  
   ## cols parameter indicate which columns store stats
-  pies <- nodepie(nodes, cols=2:12,outline.color='black',outline.size = 0.1)
+  pies <- nodepie(nodes_plot, cols=2:12,outline.color='black',outline.size = 0.1)
   pies <- lapply(pies, function(g) g+scale_fill_manual(values = egglevs$col,breaks=egglevs$Egg))
   
   t3 <- full_join(t2, data.frame(label = names(phenotypes), stat = phenotypes ), by = 'label')
   tp <- ggtree(t3,layout='rectangular',branch.length = 'none') %<+% md
   tp$data$dummy <- 1
-  tp_final <- tp + geom_inset(pies, width = .09, height = .09)+ 
-    geom_tippoint(aes(fill=Egg),pch=21,size=1)+
-    scale_fill_manual(values=egglevs$col,breaks=egglevs$Egg) 
-  assign(paste0(tree,'_pies'),tp_final)
-  
+  tp_final <- tp + geom_inset(pies, width = .09, height = .09)
+  tp_phenos <- tp_final +
+    geom_tippoint(aes(fill=Hap),pch=21,size=1.5)+
+    scale_fill_manual(values=md$HapCol,breaks=md$Hap)
+  assign(paste0(tree,'_nodes'),tp_final)
+  assign(paste0(tree,'_pies'),tp_phenos)
 }
+
+# Ensure ER fits better than SYM
+# First, look at AIC for ER vs SYM models:
+# Fit the SYM model
+fitSYM <- ape::ace(phenotypes, t2, model = "SYM", type = "discrete")
+logLik_SYM <- fitSYM$loglik # Extract the log-likelihood
+num_states <- ncol(fitSYM$lik.anc) # SYM has (s * (s - 1)) / 2 # rate parameters, s = the number of states
+k_SYM <- (num_states * (num_states - 1)) / 2  # rate parameters
+k_SYM <- k_SYM + (num_states - 1)  # add base frequencies
+AIC_SYM <- 2 * k_SYM - 2 * logLik_SYM
+AIC_SYM
+
+# Fit the ER model
+fitER <- ape::ace(phenotypes, t2, model = "ER", type = "discrete")
+logLik_ER <- fitER$loglik
+k_ER <- 1 + (ncol(fitER$lik.anc) - 1)  # base frequencies, there is 1 transition rate (k = 1) plus the base frequencies (number of states - 1)
+AIC_ER <- 2 * k_ER - 2 * logLik_ER
+cat('AIC for ER: ', AIC_ER, ' AIC for SYM: ',AIC_SYM,'\n')
+# ER fits better 
 
 # Grab the model results
 full_search_res <- rbindlist(full_tree_results)
@@ -219,15 +251,146 @@ full_search_res <- read_tsv('Full_Search_Results_20240806.txt')
 # Plot all connections 
 discord <- simple.tanglegram(tree1=mt_tree_pies,tree2=a_tree_pies,column=dummy,value=1,t2_pad=2,l_color='black',tiplab=F)
 
-pdf('../../figures/20240717_TreeCompare-mtDNA-Auto-NodePiesML.pdf',height=5,width=7)
+pdf('../../figures/20240905_TreeCompare-mtDNA-Auto-NodePiesML.pdf',height=5,width=7)
 discord
 dev.off()
 
 # Swap 
 discord2 <- simple.tanglegram(tree1=a_tree_pies,tree2=mt_tree_pies,column=dummy,value=1,t2_pad=2,l_color='black',tiplab=F)
 
-pdf('../../figures/20240717_TreeCompare-Auto-mtDNA-NodePiesML.pdf',height=5,width=7)
+pdf('../../figures/20240905_TreeCompare-Auto-mtDNA-NodePiesML.pdf',height=5,width=7)
 discord2
+dev.off()
+
+# Instead of hap colors for tips, do egg - we need tips so need to repeat this x4! 
+### EGG! ### 
+mt_egg <- mt_tree_nodes +
+  geom_tippoint(aes(fill=Egg),pch=22,size=1.5)+
+  scale_fill_manual(values=egglevs$col,breaks=egglevs$Egg)
+a_egg <- a_tree_nodes +
+  geom_tippoint(aes(fill=Egg),pch=22,size=1.5)+
+  scale_fill_manual(values=egglevs$col,breaks=egglevs$Egg)
+discord_egg <- simple.tanglegram(tree1=mt_egg,tree2=a_egg,column=dummy,value=1,t2_pad=2,l_color='black',tiplab=F)
+discord_egg2 <- simple.tanglegram(tree1=a_egg,tree2=mt_egg,column=dummy,value=1,t2_pad=2,l_color='black',tiplab=F)
+
+
+pdf('../../figures/20240905_TreeCompare-mtDNA-Auto-NodePiesML-EGG.pdf',height=5,width=7)
+discord_egg
+dev.off()
+
+pdf('../../figures/20240905_TreeCompare-Auto-mtDNA-NodePiesML-EGG.pdf',height=5,width=7)
+discord_egg2
+dev.off()
+
+### K=5! ### 
+#add same custom grey color scale 
+kcols = data.frame(Kcols = gray.colors(5,start=0.05,end=0.95)[c(1,4,3,5,2)], AncestryK5 = paste0('K',seq(1,5,1)))
+
+mt_k5 <- mt_tree_nodes +
+  geom_tippoint(aes(fill=AncestryK5),pch=22,size=1.5)+
+  scale_fill_manual(values=kcols$Kcols,breaks=kcols$AncestryK5)
+a_k5 <- a_tree_nodes +
+  geom_tippoint(aes(fill=AncestryK5),pch=22,size=1.5)+
+  scale_fill_manual(values=kcols$Kcols,breaks=kcols$AncestryK5)
+discord_k5 <- simple.tanglegram(tree1=mt_k5,tree2=a_k5,column=dummy,value=1,t2_pad=2,l_color='black',tiplab=F)
+discord_k52 <- simple.tanglegram(tree1=a_k5,tree2=mt_k5,column=dummy,value=1,t2_pad=2,l_color='black',tiplab=F)
+
+pdf('../../figures/20240905_TreeCompare-mtDNA-Auto-NodePiesML-K5.pdf',height=5,width=7)
+discord_k5
+dev.off()
+
+pdf('../../figures/20240905_TreeCompare-Auto-mtDNA-NodePiesML-K5.pdf',height=5,width=7)
+discord_k52
+dev.off()
+
+### Hap! ### 
+mt_hap <- mt_tree_nodes +
+  geom_tippoint(aes(fill=Hap),pch=22,size=1.5)+
+  scale_fill_manual(values=md$HapCol,breaks=md$Hap)
+a_hap <- a_tree_nodes +
+  geom_tippoint(aes(fill=Hap),pch=22,size=1.5)+
+  scale_fill_manual(values=md$HapCol,breaks=md$Hap)
+discord_hap <- simple.tanglegram(tree1=mt_hap,tree2=a_hap,column=dummy,value=1,t2_pad=2,l_color='black',tiplab=F)
+discord_hap2 <- simple.tanglegram(tree1=a_hap,tree2=mt_hap,column=dummy,value=1,t2_pad=2,l_color='black',tiplab=F)
+
+
+pdf('../../figures/20240905_TreeCompare-mtDNA-Auto-NodePiesML-HAP.pdf',height=5,width=7)
+discord_hap
+dev.off()
+
+pdf('../../figures/20240905_TreeCompare-Auto-mtDNA-NodePiesML-HAP.pdf',height=5,width=7)
+discord_hap2
+dev.off()
+
+### Host! ###  
+hosts <- md %>% filter(ID %in% mt_tree$tip.label) %>% select(HostParentShort,HostShape,HostColor) %>% unique %>% arrange(HostParentShort) %>% 
+  mutate(HShape = 
+           case_when(
+    HostShape == 15 ~ 22,
+    HostShape == 16 ~ 21,
+    HostShape == 17 ~ 24,
+    HostShape == 18 ~ 23,
+    TRUE ~ 8
+  ))
+mt_host <- mt_tree_nodes +
+  geom_tippoint(aes(fill=HostParentShort,shape=HostParentShort),size=1.5)+
+  scale_fill_manual(values=hosts$HostColor,breaks=hosts$HostParentShort)+
+  scale_shape_manual(values=hosts$HShape,breaks=hosts$HostParentShort)
+a_host <- a_tree_nodes +
+  geom_tippoint(aes(fill=HostParentShort,shape=HostParentShort),size=1.5)+
+  scale_fill_manual(values=hosts$HostColor,breaks=hosts$HostParentShort)+
+  scale_shape_manual(values=hosts$HShape,breaks=hosts$HostParentShort)
+discord_host <- simple.tanglegram(tree1=mt_host,tree2=a_host,column=dummy,value=1,t2_pad=2,l_color='black',tiplab=F)
+discord_host2 <- simple.tanglegram(tree1=a_host,tree2=mt_host,column=dummy,value=1,t2_pad=2,l_color='black',tiplab=F)
+
+
+pdf('../../figures/20240905_TreeCompare-mtDNA-Auto-NodePiesML-HOST.pdf',height=5,width=7)
+discord_host
+dev.off()
+
+pdf('../../figures/20240905_TreeCompare-Auto-mtDNA-NodePiesML-HOST.pdf',height=5,width=7)
+discord_host2
+dev.off()
+
+### Geography! ###  
+geos <- md %>% filter(ID %in% mt_tree$tip.label) %>% select(KDist,KDShape,KDCol) %>% unique 
+mt_geo <- mt_tree_nodes +
+  geom_tippoint(aes(fill=KDist,shape=KDist),size=1.5)+
+  scale_fill_manual(values=geos$KDCol,breaks=geos$KDist)+
+  scale_shape_manual(values=geos$KDShape,breaks=geos$KDist)
+a_geo <- a_tree_nodes +
+  geom_tippoint(aes(fill=KDist,shape=KDist),size=1.5)+
+  scale_fill_manual(values=geos$KDCol,breaks=geos$KDist)+
+  scale_shape_manual(values=geos$KDShape,breaks=geos$KDist)
+discord_geo <- simple.tanglegram(tree1=mt_geo,tree2=a_geo,column=dummy,value=1,t2_pad=2,l_color='black',tiplab=F)
+discord_geo2 <- simple.tanglegram(tree1=a_geo,tree2=mt_geo,column=dummy,value=1,t2_pad=2,l_color='black',tiplab=F)
+
+
+pdf('../../figures/20240905_TreeCompare-mtDNA-Auto-NodePiesML-GEO.pdf',height=5,width=7)
+discord_geo
+dev.off()
+
+pdf('../../figures/20240905_TreeCompare-Auto-mtDNA-NodePiesML-GEO.pdf',height=5,width=7)
+discord_geo2
+dev.off()
+
+### Habitat! ### 
+mt_hab <- mt_tree_nodes +
+  geom_tippoint(aes(fill=Habitat),pch=22,size=1.5)+
+  scale_fill_manual(values=viridis(4))
+a_hab <- a_tree_nodes +
+  geom_tippoint(aes(fill=Habitat),pch=22,size=1.5)+
+  scale_fill_manual(values=viridis(4))
+discord_hab <- simple.tanglegram(tree1=mt_hab,tree2=a_hab,column=dummy,value=1,t2_pad=2,l_color='black',tiplab=F)
+discord_hab2 <- simple.tanglegram(tree1=a_hab,tree2=mt_hab,column=dummy,value=1,t2_pad=2,l_color='black',tiplab=F)
+
+
+pdf('../../figures/20240905_TreeCompare-mtDNA-Auto-NodePiesML-HABITAT.pdf',height=5,width=7)
+discord_hab
+dev.off()
+
+pdf('../../figures/20240905_TreeCompare-Auto-mtDNA-NodePiesML-HABITAT.pdf',height=5,width=7)
+discord_hab2
 dev.off()
 
 # And as before, add the shift data 
@@ -253,10 +416,11 @@ confs_all$Egg <- factor(confs_all$Egg,levels=eggboth$Egg)
 
 # Plot
 plot_mtaut <- mt_vs_autboth %>% ungroup %>% 
+  filter(Egg != 'Multiclass') %>% 
   ggplot(aes(x=Egg,y=shifts,fill=Egg))+
   geom_hline(yintercept=0,lty=2)+
   geom_hline(yintercept=c(-1,1),lty=3,col='darkgray')+
-  geom_errorbar(data=confs_all,width=0.2,col='black',aes(x=Egg,ymin=conf_low,ymax=conf_high),inherit.aes=FALSE,position=position_nudge(x=-0.15))+
+  geom_errorbar(data=confs_all %>% filter(Egg != 'Multiclass'),width=0.2,col='black',aes(x=Egg,ymin=conf_low,ymax=conf_high),inherit.aes=FALSE,position=position_nudge(x=-0.15))+
   geom_boxplot(width = .15,outlier.shape = NA, alpha = 0.9,lwd=0.25,position=position_nudge(x=0.15)) +
   #ggdist::stat_halfeye(width = .3,.width = 0,justification = -.2, point_colour = NA,alpha = 0.95,normalize='groups')+
   scale_fill_manual(values=eggboth$col,breaks=eggboth$Egg)+
@@ -266,8 +430,8 @@ plot_mtaut <- mt_vs_autboth %>% ungroup %>%
   coord_flip(ylim=c(-15,15))
 plot_mtaut
 
-png('../../figures/20240807_mtDNA-vs-Autosomal-shifts-MCMC-Multiclass.png',units='in',res=300,height=4,width=4)
-pdf('../../figures/20240807_mtDNA-vs-Autosomal-shifts-MCMC-Multiclass-Boxes.pdf',height=4.5,width=4)
+png('../../figures/20240905_mtDNA-vs-Autosomal-shifts-MCMC-Multiclass.png',units='in',res=300,height=4,width=4)
+pdf('../../figures/20240905_mtDNA-vs-Autosomal-shifts-MCMC-Iterative-Only-Boxes.pdf',height=4.5,width=4)
 plot_mtaut
 dev.off()
 
--- phylogenetics/trees/README.md
+++ phylogenetics/trees/README.md
@@ -8,6 +8,3 @@
 
 `chr_W_HostOG...`: These 4 annotated trees show the BEAST time dated phylogenies with difference sensitivities (e.g. constant or exponential). 
 
-`chr_MT_All.SNP.DP3-AC2-MQ40.min4.phy.contree`: used for classifying all samples into haplogroups 
-
-`chr_W_All.SNP.DP3-AC2-MQ40.min4.phy.contree`: same as above, except only higher quality W chromosome for checking. 
